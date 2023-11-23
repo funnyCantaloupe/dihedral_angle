960b661387efa8d6014ce8c541a6619efff1e925
karolaina2010 <70517826+karolaina2010@users.noreply.github.com>
2023-11-23 12:53:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 from Bio.PDB import *
 import matplotlib.pyplot as plt
 
-# Ścieżka do pliku PDB
-pdb_file = "C:/Users/karol/PycharmProjects/Katy/1mbo.pdb"  # Zmień na właściwą ścieżkę
-
-# Inicjalizacja parsera PDB
+#Sciezka do pliku, wczytanie pliku
+pdb_file = "C:/Users/karol/PycharmProjects/Katy/101m.pdb"
 parser = PDBParser()
-
-# Wczytanie struktury z pliku PDB
 structure = parser.get_structure("struktura", pdb_file)
 
-# Lista kątów phi i psi
+#Lista katow phi i psi, lista kolorow dla struktur drugorzedowych
 phi_values = []
 psi_values = []
-structure_colors = []  # Lista przechowująca kolory dla poszczególnych struktur drugorzędowych
+structure_colors = []
 
-# Iteracja po łańcuchach i resztach w strukturze
+#Przejscie po lancuchach i resztach
 for model in structure:
     for chain in model:
         polypeptides = PPBuilder().build_peptides(chain)
@@ -23,39 +19,38 @@ for model in structure:
             phi_psi = poly.get_phi_psi_list()
             for phi, psi in phi_psi:
                 if phi is not None and psi is not None:
-                    # Konwersja kątów z radianów na stopnie
+                    #Radiany na stopnie
                     phi_deg = phi * (180.0 / 3.14159)
                     psi_deg = psi * (180.0 / 3.14159)
                     phi_values.append(phi_deg)
                     psi_values.append(psi_deg)
 
-                    # Przypisanie koloru na podstawie kryteriów dla helisy alfa i arkusza beta
+                    #Kolor dla alpha helix, beta lub inne
                     if phi_deg < 0 and psi_deg < 0:
-                        structure_colors.append('blue')  # Helisa alfa - niebieski kolor
+                        structure_colors.append('blue')
                     elif phi_deg < 0 and psi_deg > 0:
-                        structure_colors.append('red')  # Arkusz beta - czerwony kolor
+                        structure_colors.append('red')
                     else:
-                        structure_colors.append('green')  # Pozostałe - zielony kolor
+                        structure_colors.append('green')
 
-# Stworzenie wykresu Ramachandrana z kolorami dla struktur drugorzędowych
+#Wykres Ramachandrana
 plt.figure(figsize=(8, 6))
 for color, phi, psi in zip(structure_colors, phi_values, psi_values):
     plt.scatter(phi, psi, s=5, alpha=0.5, c=color)
 
-plt.title('Mapa Ramachandrana z kolorem dla struktur drugorzędowych')
-plt.xlabel('Kąt Phi (°)')
-plt.ylabel('Kąt Psi (°)')
+plt.title('Mapa Ramachandrana z kolorem dla struktur drugorzedowych')
+plt.xlabel('Kat Phi (°)')
+plt.ylabel('Kat Psi (°)')
 plt.xlim(-180, 180)
 plt.ylim(-180, 180)
 plt.grid(True)
 
-# Legenda dla typów struktury drugorzędowej
-legend_elements = [plt.Line2D([0], [0], marker='o', color='w', label='Helisa alfa',
+#Legenda
+legend_elements = [plt.Line2D([0], [0], marker='o', color='w', label='Alpha Helix',
                               markerfacecolor='blue', markersize=8),
-                   plt.Line2D([0], [0], marker='o', color='w', label='Arkusze beta',
+                   plt.Line2D([0], [0], marker='o', color='w', label='Beta Sheet',
                               markerfacecolor='red', markersize=8),
                    plt.Line2D([0], [0], marker='o', color='w', label='Inne',
                               markerfacecolor='green', markersize=8)]
 plt.legend(handles=legend_elements)
-
 plt.show()
