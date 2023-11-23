250de52e2368f14b0148c347018699b21c9b20ba
karolaina2010 <70517826+karolaina2010@users.noreply.github.com>
2023-11-23 12:54:11 +0100

    Update dihedral.py

--- dihedral.py
+++ dihedral.py
@@ -1,16 +1,12 @@
 from Bio.PDB import *
 import numpy as np
 
-# Ścieżka do pliku PDB
-pdb_file = "C:/Users/karol/PycharmProjects/Katy/1ehz.pdb"  # Zmień na właściwą ścieżkę
-
-# Inicjalizacja parsera PDB
+#Sciezka do pliku, wczytanie pliku
+pdb_file = "C:/Users/karol/PycharmProjects/Katy/1ehz.pdb"
 parser = PDBParser()
-
-# Wczytanie struktury z pliku PDB
 structure = parser.get_structure("struktura", pdb_file)
 
-# Definicja atomów dla kątów torsyjnych
+#Definicja atomow dla katow torsyjnych - slownik, lista katow
 torsion_atoms = {
     'alpha': ('O3\'', 'P', 'O5\'', 'C5\''),
     'beta': ('P', 'O5\'', 'C5\'', 'C4\''),
@@ -22,26 +18,25 @@ torsion_atoms = {
     'chi_pyrimidines': ('O4\'', 'C1\'', 'N1', 'C2')
 }
 
-# Lista kątów torsyjnych
 torsion_angles = []
 
-# Iteracja po łańcuchach i resztach w strukturze
+#Przejscie po resztach i lancuchach w strukturze
 for model in structure:
     for chain in model:
         residues = list(chain.get_residues())
         for i in range(len(residues)):
             residue = residues[i]
-            residue_number = residue.get_id()[1]  # Numer reszty
+            residue_number = residue.get_id()[1]
 
-            torsion_values = [f"{residue_number}"]  # Numer reszty jako pierwsza wartość
+            torsion_values = [f"{residue_number}"]
 
             for angle, atoms in torsion_atoms.items():
                 try:
                     atom1, atom2, atom3, atom4 = atoms
 
-                    # Jeśli obliczamy kąt alfa i poprzednia reszta istnieje, atom1 powinien pochodzić z poprzedniej reszty
+                    #Dla alpha atom1 powinien byc z poprzedniej reszty, pierwszy alpha jest None
                     if angle == 'alpha' and i == 0:
-                        torsion_values.append(None)  # Dla pierwszej reszty kąt alfa jest None
+                        torsion_values.append(None)
                         continue
 
                     if angle == 'alpha' and i > 0:
@@ -49,44 +44,52 @@ for model in structure:
                     else:
                         atom1 = residue[atom1]
 
-                    # Pobieranie atomów dla kąta torsyjnego
                     atom2 = residue[atom2]
-                    #atom3 = residue[atom3]
 
+                    #Dla zeta atom3 powinien byc z nastepnej reszty
                     if angle == 'zeta' and i < len(residues) - 1:
                         atom3 = residues[i + 1][atom3]
                     else:
                         atom3 = residue[atom3]
 
+                    #Dla epsilon i zeta atom4 powinien byc z nastepnej reszty
                     if angle in ['epsilon', 'zeta'] and i < len(residues) - 1:
                         atom4 = residues[i + 1][atom4]
                     else:
                         atom4 = residue[atom4]
 
-                    # Obliczanie kąta torsyjnego
+                    #Obliczanie kata torsyjnego
                     torsion_angle = calc_dihedral(atom1.get_vector(), atom2.get_vector(),
                                                   atom3.get_vector(), atom4.get_vector())
 
                     torsion_values.append(np.degrees(torsion_angle))
+
                 except KeyError:
                     torsion_values.append(None)
             torsion_angles.append(torsion_values)
 
-            # Iteracja przez każdą linijkę torsion_values
+            #Kat chi, jesli puryna to nie pirydymina
             for values in torsion_values:
-                # Sprawdzenie, czy wartość torsion_values[7] nie jest None i czy długość listy jest wystarczająca
                 if torsion_values[7] is not None:
-                    # Zmiana wartości torsion_values[8] na None
                     torsion_values[8] = None
 
-            #print(torsion_values)
-
-# Zapisanie wyników do pliku CSV jako macierz n x m z wartościami zaokrąglonymi do jednego miejsca po przecinku
-with open('kąty_torsyjne.csv', 'w') as file:
+#Zapisanie wynikow
+with open('katy_torsyjne.csv', 'w') as file:
     header = ",".join([angle for angle in torsion_atoms.keys()])
     file.write(f"Reszta,{header}\n")
 
     for angle_row in torsion_angles:
-        # Zaokrąglanie wartości do jednego miejsca po przecinku
         line = ",".join([f"{value:.1f}" if isinstance(value, float) else str(value) for value in angle_row])
         file.write(f"{line}\n")
+
+
+pdb_file2 = "C:/Users/karol/PycharmProjects/Katy/katy_torsyjne.csv"
+with open(pdb_file2, 'r+') as file:
+    lines = file.readlines()
+    file.seek(0)
+
+    for line in lines[:77]:
+        file.write(line)
+
+    file.truncate()
+
